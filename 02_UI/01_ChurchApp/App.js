9352c35a26b644e7be189d59ef0f1b7eff054c96
noni1016 <play.noni@gmail.com>
2021-03-27 13:11:07 +0900

    App.js is updated to simple version for test

--- 02_UI/01_ChurchApp/App.js
+++ 02_UI/01_ChurchApp/App.js
@@ -7,39 +7,11 @@
  */
 
 import React, {useEffect, useState} from 'react';
-import {
-  ActivityIndicator,
-  FlatList,
-  Text,
-  View
-} from 'react-native';
-
+import { Text } from 'react-native';
 
 const App = () => {
-
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('https://reactnative.dev/movies.json')
-      .then((response) => response.json())
-      .then((json) => setData(json.movies))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
-
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>{item.title}, {item.releaseYear}</Text>
-          )}
-        />
-      )}
-    </View>
+    <Text>Hello sjaCoders~</Text>
   );
 };
 
