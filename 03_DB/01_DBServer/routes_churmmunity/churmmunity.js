8d3f26c6761c89d270b667abbccf4912f9e7cf8f
PiServer <sjaCoders@gmail.com>
2021-08-22 14:20:00 +0900

    그룹에 속한 멤버 가져오는 request 추가

--- 03_DB/01_DBServer/routes_churmmunity/churmmunity.js
+++ 03_DB/01_DBServer/routes_churmmunity/churmmunity.js
@@ -12,11 +12,21 @@ router.get('/', function (req, res, next) {
     res.json('churmmunity route / ');
 });
 
-router.get('/getMyGroupDatas', (req, res) => {
-    let sql = 'SELECT * FROM myGroupDatas'; // myGroupDatas 테이블에서 모든 값을 가져옴
+// [CUHR-94] myGroupDatas, RecGroupDatas 정보를 Groups 로 합침
+
+router.post('/GetMyGroupDatas', (req, res) => {
+    // let sql = 'SELECT * FROM myGroupDatas'; // myGroupDatas 테이블에서 모든 값을 가져옴
+    // console.log('request on');
+    // console.log(req.body);
+    let sql = `SELECT Groups.id, Groups.name, Groups.mainImg, Groups.location, Groups.numMember, Groups.description
+    FROM Groups, GroupUser, User
+    WHERE GroupUser.userId = ${req.body.userId}
+        AND GroupUser.userId = User.id
+        AND GroupUser.groupId = Groups.id`;
     conn.query(sql, function (error, rows, fields) { // sql 쿼리 수행
         if (!error) {
-            console.log(rows);
+            // console.log(rows);
+            console.log('query success')
             res.send(rows);
         } else {
             console.log('query error : ' + error);
@@ -24,11 +34,28 @@ router.get('/getMyGroupDatas', (req, res) => {
     });
 });
 
-router.get('/getMyLightDatas', (req, res) => {
+router.post('/GetGroupMembers', (req, res) => {
+    let sql = `SELECT User.id, User.name, User.photo
+    FROM Groups, GroupUser, User
+    WHERE GroupUser.groupId = ${req.body.groupId}
+        AND GroupUser.userId = User.id
+        AND GroupUser.groupId = Groups.id`;
+    conn.query(sql, function (error, rows, fields) { // sql 쿼리 수행
+        if (!error) {
+            // console.log(rows);
+            console.log('query success')
+            res.send(rows);
+        } else {
+            console.log('query error : ' + error);
+        }
+    });
+})
+
+router.get('/GetMyLightDatas', (req, res) => {
     let sql = 'SELECT * FROM myLightDatas'; // myLightDatas 테이블에서 모든 값을 가져옴
     conn.query(sql, function (error, rows, fields) { // sql 쿼리 수행
         if (!error) {
-            console.log(rows);
+            // console.log(rows);
             res.send(rows);
         } else {
             console.log('query error : ' + error);
@@ -36,7 +63,7 @@ router.get('/getMyLightDatas', (req, res) => {
     });
 });
 
-router.get('/getRecGroups', (req, res) => {
+router.get('/GetRecGroups', (req, res) => {
     let sql = 'SELECT * FROM recGroups'; // recGroups 테이블에서 모든 값을 가져옴
     conn.query(sql, function (error, rows, fields) { // sql 쿼리 수행
         if (!error) {
@@ -49,11 +76,11 @@ router.get('/getRecGroups', (req, res) => {
 });
 
 
-router.get('/getRecGroupsOrderRand', (req, res) => {
-    let sql = 'SELECT * FROM recGroups ORDER BY RAND()'; // recGroups 테이블에서 모든 값을 가져옴
+router.get('/GetRecGroupsOrderRand', (req, res) => {
+    let sql = 'SELECT * FROM Groups ORDER BY RAND()'; // recGroups 테이블에서 모든 값을 가져옴
     conn.query(sql, function (error, rows, fields) { // sql 쿼리 수행
         if (!error) {
-            console.log(rows);
+            // console.log(rows);
             res.send(rows);
         } else {
             console.log('query error : ' + error);
@@ -62,11 +89,11 @@ router.get('/getRecGroupsOrderRand', (req, res) => {
 });
 
 
-router.get('/getRecLights', (req, res) => {
+router.get('/GetRecLights', (req, res) => {
     let sql = 'SELECT * FROM recLights'; // recLights 테이블에서 모든 값을 가져옴
     conn.query(sql, function (error, rows, fields) { // sql 쿼리 수행
         if (!error) {
-            console.log(rows);
+            // console.log(rows);
             res.send(rows);
         } else {
             console.log('query error : ' + error);
@@ -76,14 +103,14 @@ router.get('/getRecLights', (req, res) => {
 
 var today = '2021-05-15 01:00:00';
 
-router.get('/getRecLightsOrderTime', (req, res) => {
+router.get('/GetRecLightsOrderTime', (req, res) => {
     let sql = `SELECT * FROM recLights WHERE DATE_FORMAT(time, "%Y-%m-%d %H:%i:%s") > '${today}' ORDER BY time`; // recLights 테이블에서 현재 시간 이후의 값을 가져옴
     // let sql = `SELECT * FROM recLights WHERE time > '${today}' ORDER BY time`; // recLights 테이블에서 현재 시간 이후의 값을 가져옴
     // let sql = `SELECT * FROM recLights WHERE time > NOW() ORDER BY time`; // recLights 테이블에서 현재 시간 이후의 값을 가져옴
     conn.query(sql, function (error, rows, fields) { // sql 쿼리 수행
         if (!error) {
-            console.log(rows);
-            console.log(sql);
+            // console.log(rows);
+            // console.log(sql);
             res.send(rows);
         } else {
             console.log('query error : ' + error);
