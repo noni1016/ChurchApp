065dc7d8417b18f1adb7835fe8c6dbbabec8e81a
noni1016 <play.noni@gmail.com>
2021-09-04 11:21:10 +0900

    'CHUR-34' react-native-seoul/react-native-kakao-login install & build
    *뭔지 모르겠음 주의

--- 02_UI/01_ChurchApp/src/Screen/Semin.js
+++ 02_UI/01_ChurchApp/src/Screen/Semin.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text} from 'react-native';
 import Styled from 'styled-components/native';
+import {
+    KakaoOAuthToken,
+    KakaoProfile,
+    getProfile as getKakaoProfile,
+    login,
+    logout,
+    unlink,
+  } from '@react-native-seoul/kakao-login';
+
+  const Body = Styled.TouchableOpacity`
+  height: 100px;
+  width: 100px;
+  background-color: #00FF00;
+`;
 
 const Semin = () => {
+    const [result, setResult] = useState('');
+
+    const signInWithKakao = async () => {
+        const token = await login();
+    
+        setResult(JSON.stringify(token));
+      };
+
     return (
-        <Text>Hello Semin~</Text>
+        <Body onPress={() => signInWithKakao()}>
+            <Text>{result}</Text>
+        </Body>
+        
     );
   };
   
