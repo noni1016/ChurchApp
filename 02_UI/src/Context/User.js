46d80cd1d029ff778381d306a897648e3d039e46
semin0427 <pillar3500@gmail.com>
2021-12-11 14:53:30 +0900

    로딩 임시 작업

--- 02_UI/src/Context/User.js
+++ 02_UI/src/Context/User.js
@@ -2,6 +2,13 @@ import React, {useState, createContext} from 'react';
 
 const initData = {id: 3, name: '노니', photo: 'Profile/노니.jpg', church: '유목민교회', age: 29, level: 100, role: 'Admin'};
 
+//인증 시도를 했는지 여부
+const UserAuthCheckFlag = createContext({
+    authCheckFlag : false,
+    setAuthCheckFlag: () => {},
+});
+
+//인증 성공 여부
 const UserAuthChecker = createContext({
     authChecker : false,
     setAuthChecker: () => {},
@@ -14,14 +21,17 @@ const AuthContext = createContext({
 
 const UserContextProvider = ({children}) => {
     const [authChecker, setAuthChecker] = useState(false);
+    const [authCheckFlag, setAuthCheckFlag] = useState(false);
 
     return (
+        <UserAuthCheckFlag.Provider value = {{authCheckFlag, setAuthCheckFlag}}>
         <UserAuthChecker.Provider value = {{authChecker, setAuthChecker}}>
         <UserContext.Provider value = {initData}>
             {children}
         </UserContext.Provider>
         </UserAuthChecker.Provider>
+        </UserAuthCheckFlag.Provider>
     )
 }
 
-export {UserContext, UserAuthChecker, UserContextProvider};
+export {UserContext, UserAuthChecker, UserAuthCheckFlag, UserContextProvider};
--- 02_UI/src/Screen/Main.js
+++ 02_UI/src/Screen/Main.js
@@ -1,26 +1,28 @@
-import React, {useState, useEffect, useContext} from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { Text, Image } from 'react-native';
 import AuthPage from '~/Screen/Auth';
 import Navigator from '~/Screen/Navigator';
-import {UserAuthChecker, UserContextProvider} from '~/Context/User';
+import { UserAuthChecker, UserAuthCheckFlag, UserContextProvider } from '~/Context/User';
 import Default from './Default';
 
 const Main = () => {
-    const {authChecker} = useContext(UserAuthChecker);
+    const { authChecker } = useContext(UserAuthChecker);
+    const { authCheckFlag } = useContext(UserAuthCheckFlag)
 
-    useEffect(()=>
-    {
+    useEffect(() => {
         //console.log(`Main authChecker : ${authChecker}`);
     })
     return (
-    <>
-        {authChecker == false && <AuthPage />}
-        {authChecker && <Navigator />}
-        {/* <Navigator /> */}
-</>
+        <>
+            {authCheckFlag == false && <Image source={require(`~/Assets/Images/mainpray.jpg`)} />}
+
+            {authChecker == false && <AuthPage />}
+            {authChecker && <Navigator />}
+            {/* <Navigator /> */}
+        </>
 
         // <Text>Hello</Text>
-  );
+    );
 };
 
 
