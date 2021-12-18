c3d3c377f38a2f6b73c9ff68ac3f4542cb943250
noni1016 <play.noni@gmail.com>
2021-12-18 16:24:52 +0900

    DomainContext App.js 로 옮김

--- 02_UI/src/App.js
+++ 02_UI/src/App.js
@@ -2,13 +2,16 @@ import React, {useState, useEffect, useContext} from 'react';
 import { Text } from 'react-native';
 import Main from './Screen/Main';
 import {UserAuthChecker, UserContextProvider} from '~/Context/User';
+import {DomainContext, DomainContextProvider} from '~/Context/Domain';
 
 const App = () => {
 
     return (
+      <DomainContextProvider>
         <UserContextProvider>
-                <Main />
+          <Main />
         </UserContextProvider>
+      </DomainContextProvider>
 
         // <Text>Hello</Text>
   );
