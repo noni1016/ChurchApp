d0196edef3a0ac733805f295622fef45f2e7874d
noni1016 <play.noni@gmail.com>
2022-02-13 11:20:25 +0900

    Refactoring - ClubPage 수정중

--- 02_UI/src/Screen/02_Main/02_Churmmunity/Group/ClubPage.js
+++ 02_UI/src/Screen/02_Main/02_Churmmunity/Group/ClubPage.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useContext} from 'react';
-import { View, FlatList, Text, Dimensions, ScrollView, Image, NativeSyntheticEvent, NativeScrollEvent } from 'react-native';
-import styled from 'styled-components/native';
+import { View, Text, Dimensions, ScrollView, Image } from 'react-native';
+import Styled from 'styled-components/native';
 import {DomainContext} from '~/Context/Domain';
 import {UserContext} from '~/Context/User';
 import Tab from '~/Components/Tab';
@@ -8,23 +8,22 @@ import GroupPageHome from '~/Components/GroupPageHome';
 import Feeds from '~/Components/Feeds';
 import AddBtn from '~/Components/AddBtn';
 
-const Header = styled.View`
-    //height: 15%;
+const Header = Styled.View`
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
-    padding: 10px 0px 5px 10px; //상 우 하 좌
+    padding: 10px 0px 5px 10px; 
     background-color: #FFFFFF;
 `;
 
-const Title = styled.Text`
+const Title = Styled.Text`
     height: 30px;
     color: black;
     font-size: 27px;
     font-family: 'DoHyeon-Regular';
 `;
 
-const TabContainer = styled.SafeAreaView`
+const TabContainer = Styled.SafeAreaView`
   flex-direction: row;
   background-color: skyblue;
 `;
@@ -33,64 +32,62 @@ const ClubPage = ({route, navigation}) => {
     const domain = useContext(DomainContext);
     const user = useContext(UserContext);
     const data = route.params.club;
-    const [tabIndex, setTabIndex] = useState(0);
-    var [imgWidth, setImgWidth] = useState();
-    var [imgHeight, setImgHeight] = useState();    
-    var [resizedWidth, setResizedWidth] = useState(100);
-    var [resizedHeight, setResizedHeight] = useState(100);
-    var [url, setUrl] = useState('');
+    const [tabIdx, SetTabIdx] = useState(0); 
+    var [resizedWidth, SetResizedWidth] = useState(100);
+    var [resizedHeight, SetResizedHeight] = useState(100);
+    var [url, SetUrl] = useState('');
     var [refresh, SetRefresh] = useState(false);
     var tabs = ['홈', '게시글', '사진'];
 
-    var [groupMember, setGroupMember] = useState([]);
+    var [clubMember, SetClubMember] = useState([]);
     var reqMemberData = {groupId: data.id};
 
-    var [isMember, setIsMember] = useState(false);
+    var [isMember, SetIsMember] = useState(false);
     
     useEffect(() => {
-        setUrl(`${domain}/${data.mainImg}`);
+        SetUrl(`${domain}/${data.mainImg}`);
     }, []);    
 
     useEffect(() => {
-        if (route.params.tabIdx) setTabIndex(route.params.tabIdx);
-        else setTabIndex(0);
+        Image.getSize(url, (width, height) => {
+            if (width > Dimensions.get('window').width) {
+                SetResizedWidth(Dimensions.get('window').width);
+                SetResizedHeight(Dimensions.get('window').width / width * height);
+            } else {
+                SetResizedWidth(width);
+                SetResizedHeight(height);
+            }
+        }, () => { console.log(`fail to get imgSize : ${url}`) })
+    }, [url])
+
+    useEffect(() => {
+        if (route.params.tabIdx) SetTabIdx(route.params.tabIdx);
+        else SetTabIdx(0);
     }, [route.params.tabIdx]);
 
     useEffect(() => {
         console.log('route.params.edit: ' + route.params.edit);
-        // if (route.params.edit || tabIndex) SetRefresh(!refresh);
         SetRefresh(!refresh);
         console.log('refresh: ' + refresh);
-    }, [route.params.edit, tabIndex]);
+    }, [route.params.edit, tabIdx]);
 
     useEffect(() => {
-            Image.getSize(url, (width, height) => {
-                setImgWidth(width);
-                setImgHeight(height);
-                if (width > Dimensions.get('window').width) {
-                    setResizedWidth(Dimensions.get('window').width);
-                    setResizedHeight(Dimensions.get('window').width / width * height);
-                } else {
-                    setResizedWidth(width);
-                    setResizedHeight(height);
-                }
-            }, () => {console.log(`fail to get imgSize : ${url}`)})
-    }, [url])
+        fetch(`${domain}/Club/${data.id}/Member`).then(res => res.json()).then(res => {SetClubMember(res);});
+    }, [data])
 
-    const GetGroupMember = () => {
-        fetch(domain + '/Churmmunity/GetGroupMembers', {
-            method: 'POST',
-            body: JSON.stringify(reqMemberData),
-            headers:{
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
+    useEffect(() => {
+        // 이 그룹 멤버인지 찾기
+        clubMember.map((member, index) => {
+            if (member.id === user.id) {
+                SetIsMember(true);
             }
-        }).then(res => res.json()).then(res => {setGroupMember(res);});
-    };
+        })
+        console.log('isMember: ',isMember);
+    }, [clubMember])
 
     //DB Groups.numMember 저장 요청
     const SetNumGroupMemberDB = () => {
-        let sendNumMemberData = {groupId: data.id, numMember: groupMember.length};
+        let sendNumMemberData = {groupId: data.id, numMember: clubMember.length};
         console.log('SetNumGroupMemberDB Called!')
         fetch(domain + '/Churmmunity/SetNumGroupMember', {
             method: 'POST',
@@ -102,23 +99,9 @@ const ClubPage = ({route, navigation}) => {
         }).then(res => res.json()).then(res => console.log(res));
     };
 
-    useEffect(() => {
-        GetGroupMember();
-    }, [data])
-
-    useEffect(() => {
-        // 이 그룹 멤버인지 찾기
-        groupMember.map((member, index) => {
-            if (member.id === user.id) {
-                setIsMember(true);
-            }
-        })
-        console.log('isMember: ',isMember);
-    }, [groupMember])
-
     const SetMember = (curUserIsMemOfThisGroup) => {
         console.log('SetMember Called!')
-        setIsMember(curUserIsMemOfThisGroup);
+        SetIsMember(curUserIsMemOfThisGroup);
         GetGroupMember();
         SetNumGroupMemberDB();
     };
@@ -138,20 +121,20 @@ const ClubPage = ({route, navigation}) => {
                     {tabs.map((label, index) => (
                         <Tab
                             key={`tab-${index}`}
-                            selected={tabIndex === index}
+                            selected={tabIdx === index}
                             label={label}
                             onPress={() => {
-                                setTabIndex(index);
+                                SetTabIdx(index);
                             }}
                         />
                     ))}
                 </TabContainer>
-                {tabIndex == 0 && <GroupPageHome data={data} groupMem={groupMember} isMember={isMember} setMember={(value)=>{SetMember(value)}}/>}
-                {tabIndex == 1 && <Feeds groupId={data.id} feedAdded={refresh} navigation={navigation}/>}
-                {tabIndex == 2 && <Text>사진</Text>}
+                {tabIdx == 0 && <GroupPageHome data={data} groupMem={clubMember} isMember={isMember} setMember={(value)=>{SetMember(value)}}/>}
+                {tabIdx == 1 && <Feeds groupId={data.id} feedAdded={refresh} navigation={navigation}/>}
+                {tabIdx == 2 && <Text>사진</Text>}
                 <Text>{user.name}</Text>
             </ScrollView>
-            {tabIndex == 1 && <AddBtn OnPressMethod={() => {navigation.navigate('AddFeed', {groupData: data, navigation: navigation});}}/>}
+            {tabIdx == 1 && <AddBtn OnPressMethod={() => {navigation.navigate('AddFeed', {groupData: data, navigation: navigation});}}/>}
         </View>
     )
 };
