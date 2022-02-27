bbd655eb2237bb97855ddb50ff591e61069f85a2
PiServer <sjaCoders@gmail.com>
2022-02-27 19:50:08 +0900

    'CHUR-113' 서버 Rest API 적용중 2 - Club 가입시 멤버 수 세팅

--- 03_Server/routes/03_Churmmunity/Club.js
+++ 03_Server/routes/03_Churmmunity/Club.js
@@ -75,40 +75,61 @@ router.get('/:clubId/NumMember/', function (req, res) {
 })
 
 // Join club
-router.get('/:clubId/Join/:userId', (req, res) => {
+router.get('/:clubId/Join/:userId', async (req, res) => {
+    let cnt = 0;
+    let sql1 = `INSERT INTO ClubUser (clubId, userId, role) VALUES (${req.params.clubId}, ${req.params.userId}, 'user')`;
+    let sql2 = `SELECT COUNT(*) FROM Club, ClubUser, User WHERE ClubUser.clubId = ${req.params.clubId}
+        AND ClubUser.userId = User.id
+        AND ClubUser.clubId = Club.id`;
+    let sql3 = ``;
+    console.log(sql1);
+    console.log(sql2);
+    try {
+        await conn.beginTransaction();
+        await conn.query(sql1);
+        await conn.query(sql2, (error, rows) => {
+            let row = rows[0];
+            cnt = row['COUNT(*)'];       
+            console.log(cnt);     
+            sql3 = `Update Club SET numMember = ${cnt} WHERE id = ${req.params.clubId}`;
+            conn.query(sql3);
+        });
+        console.log(sql3);
+        await conn.commit();
+        res.send('OK');
+    } catch (err) {
+        console.log(err)
+        await conn.rollback()
+        res.status(500).json(err)
+    }
+})
+
+
+
+// Exit club
+router.get('/:clubId/Join/:userId', async (req, res) => {
     let sql1 = `INSERT INTO ClubUser (clubId, userId, role) VALUES (${req.params.clubId}, ${req.params.userId}, 'user')`;
     let sql2 = `SELECT COUNT(*) FROM Club, ClubUser, User WHERE ClubUser.clubId = ${req.params.clubId}
         AND ClubUser.userId = User.id
         AND ClubUser.clubId = Club.id`;
     console.log(sql1);
     console.log(sql2);
-    conn.query(sql1 + sql2, function (error, rows, fields) { // sql 쿼리 수행
-        if (!error) {
-            // console.log(rows);
-            // console.log('query success')
-            var res1 = rows[0];	//sql1 의 결과값
-            var res2 = rows[1];	//sql2 의 결과값
-            let sql3 = `UPDATE Club SET numMember = ${rows[1]} WHERE Club.id = ${req.params.clubId}`;
-            
-            conn.query(sql3, function (error, rows, fields) { // sql 쿼리 수행
-                if (!error) {        
-                    res.send('success');
-                } else {
-                    console.log('query error : ' + error);
-                    res.send('fail' + error);
-                }
-            });
-
-            res.send(rows[1]);
-        } else {
-            console.log('query error : ' + error);
-            res.send('fail' + error);
-        }
-    });
+    try {
+        await conn.beginTransaction();
+        // const ins = await conn.query(`INSERT INTO ClubUser set ?`, {clubId: req.params.clubId, userId: req.params.userId, role: 'user'});
+        // const cnt = await conn.query(`UPDATE Club set numMember = numMember + 1 where id = ?`, [req.params.clubId]);
+        
+        await conn.query(sql1);
+        let cnt = await conn.query(sql2);
+        console.log('cnt :'+ cnt[0]);
+        await conn.commit();
+        res.send('OK');
+    } catch (err) {
+        console.log(err)
+        await conn.rollback()
+        res.status(500).json(err)
+    }
 })
-
-
-// Exit club
 
 // const updateClubMem
 
