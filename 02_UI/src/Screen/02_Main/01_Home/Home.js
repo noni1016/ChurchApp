6f0a24cd100254ae442b6b539a12b6b0925e5f82
semin0427 <pillar3500@gmail.com>
2022-03-01 15:14:53 +0900

    메인페이지에 내가나왔으면 정말좋겠네- 정말좋겠네

--- 02_UI/src/Screen/02_Main/01_Home/Home.js
+++ 02_UI/src/Screen/02_Main/01_Home/Home.js
@@ -2,17 +2,22 @@ import React, {useContext, useEffect} from 'react';
 import { View, FlatList, Text, Dimensions, ScrollView, Image, NativeSyntheticEvent, NativeScrollEvent } from 'react-native';
 import Styled from 'styled-components/native';
 import AddBtn from '~/Components/AddBtn'
-import { AuthContext, UserContextProvider } from '~/Context/User';
+import { UserData } from '~/Context/User';
 
 const data = {id: 5};
 
 const Home = () => {
-
+    const { userData, setUserData } = useContext(UserData);
     return (
-        <View>
-            {/* <AddBtn OnPressMethod={() => {alert('AddBtn')}}/> */}
+        <>
+        {userData != null && <View>
+            <Image style={{ width: '100%', height: '100%', resizeMode: 'contain' }} source={{ uri: userData.photo }} />
+          </View>}
+          
+        {userData == null && <View>
             <Image source={require(`~/Assets/Images/bakery.jpg`)}/>
-        </View>
+            </View>}
+        </>
     )
 }
 
