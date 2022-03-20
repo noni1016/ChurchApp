30a63b402ed585e82d121fc20948ebb7228c03bf
noni1016 <play.noni@gmail.com>
2022-03-20 14:32:25 +0900

    'CHUR-119' GPS 데이터 가져오기

--- 02_UI/src/Screen/02_Main/01_Home/Home.js
+++ 02_UI/src/Screen/02_Main/01_Home/Home.js
@@ -1,16 +1,41 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { View, FlatList, Text, Dimensions, ScrollView, Image, NativeSyntheticEvent, NativeScrollEvent } from 'react-native';
 import Styled from 'styled-components/native';
 import AddBtn from '~/Components/AddBtn'
 import { UserData } from '~/Context/User';
+import Geolocation from 'react-native-geolocation-service';
 
 const data = {id: 5};
 
 const Home = () => {
     const { userData, setUserData } = useContext(UserData);
+    const [location, setLocation] = useState(undefined);
+
+    useEffect(() => {
+        Geolocation.getCurrentPosition(
+            position => {
+                const {latitude, longitude} = position.coords;
+                setLocation({
+                    latitude,
+                    longitude,
+                });
+            },
+            error => {
+                console.log(error.code, error.message);
+            },
+            {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+        );
+    },[])
+    
     
     return (
         <>
+        {location ? (
+            <>
+            <Text>Latitude: {location.latitude}</Text>
+            <Text>Longitude: {location.longitude}</Text>
+            </>
+        ) : (<Text>Loading...</Text>)}
         {userData != null && <View>
             <Image style={{ width: '100%', height: '100%', resizeMode: 'contain' }} source={{ uri: userData.photo + "?cache="+Math.random() }} />
           </View>}
@@ -19,6 +44,8 @@ const Home = () => {
             <Image source={require(`~/Assets/Images/bakery.jpg`)}/>
             </View>}
         </>
+
+
     )
 }
 
