117dd41275044a9e585e877d9afa964b88d0df99
PiServer <sjaCoders@gmail.com>
2022-07-30 15:01:07 +0900

    'CHUR-121' 새 공동체 만들기 - Server #3 ClubUser 정보 추가 BE

--- 03_Server/routes/03_Churmmunity/Group.js
+++ 03_Server/routes/03_Churmmunity/Group.js
@@ -30,9 +30,12 @@ const uploadGroupImg = multer({
 //     res.send({fileName: `${req.file.filename}`});
 // })
 
-router.post('/:type', (req, res) => {
-    let sql = ``;
-    uploadGroupImg(req, res, (err) => {
+router.post('/:type', async (req, res) => {
+    let sql1 = ``;
+    let sql2 = ``;
+    let sql3 = ``;
+    let clubId;
+    uploadGroupImg(req, res, async (err) => {
         if (err) {
             res.send({fileName : null});
         }
@@ -44,25 +47,57 @@ router.post('/:type', (req, res) => {
         {
             console.log('Create Club');
             // sql = `INSERT INTO Club (name, mainImg, location, description) VALUES ('${req.body.name}', '${req.file.filename}', '${req.body.location}', '${req.body.description}')` ;
-            sql = `INSERT INTO Club (name, mainImg, location, description) VALUES ('${req.body.name}', 'GroupImg/${req.file.filename}', '군포시 수리산로', '${req.body.description}')` ;
+            sql1 = `INSERT INTO Club (name, mainImg, location, description) VALUES ('${req.body.name}', 'GroupImg/${req.file.filename}', '군포시 수리산로', '${req.body.description}')` ;
+            console.log(sql1);
+            try {
+                await conn.beginTransaction();
+                await conn.query(sql1, (error, rows) => {
+                    // console.log(rows);
+                    clubId = rows.insertId;
+                    sql2 = `INSERT INTO ClubUser (clubId, userId, role) VALUES (${clubId}, ${req.body.userId}, 'leader')`;
+                    console.log(sql2);
+                    conn.query(sql2, (error, rows) => {
+                        // console.log(rows);
+                        sql3 = `SELECT * FROM ClubView WHERE id = ${clubId}`;
+                        console.log(sql3);
+                        conn.query(sql3, (error, rows) => {
+                            console.log(rows[0]);
+                            conn.commit();
+                            res.json(rows[0]);
+                        })
+
+                    });
+                    
+                });
+            } catch (err) {
+                console.log(err)
+                await conn.rollback()
+                res.send({ result: false });
+            }
+    
+            
+            
         } else if (req.params.type == '2') // Spot
         {
             console.log('Create Spot');
         }
 
-        console.log(sql);
-        conn.query(sql, function (error, rows, fields) { // sql 쿼리 수행
-            if (!error) {
-                // console.log(rows);
-                // console.log('query success')
-                res.send({fileName: `${req.file.filename}`});
-            } else {
-                console.log('query error : ' + error);
-            }
-        });
+
+        // console.log(sql1);
+        // conn.query(sql1, function (error, rows, fields) { // sql 쿼리 수행
+        //     if (!error) {
+        //         // console.log(rows);
+        //         // console.log('query success')
+        //         res.send({fileName: `${req.file.filename}`});
+        //     } else {
+        //         console.log('query error : ' + error);
+        //     }
+        // });
 
     });
 
 })
+
+
 
 module.exports = router;
