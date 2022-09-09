c3c39af6fb0059e8445537352ea3397ff6446c9a
noni1016 <play.noni@gmail.com>
2022-09-09 09:59:07 +0900

    'CHUR-140' 최근 검색어 UI 작업

--- 02_UI/src/Screen/02_Main/02_Churmmunity/SearchGroups.js
+++ 02_UI/src/Screen/02_Main/02_Churmmunity/SearchGroups.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import {View,Text, ActivityIndicator, ScrollView, Button} from 'react-native';
 import Styled from 'styled-components/native';
 import ClubCard from '~/Components/ClubCard';
+import TagBox from '~/Components/TagBox';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import Icon2 from 'react-native-vector-icons/MaterialIcons';
@@ -34,6 +35,13 @@ const Separator = Styled.View`
     margin-vertical: 20px;
 `;
 
+const KeywordView = Styled.View`
+    width: 90%;
+    flex-direction: row;
+    flex-wrap: wrap;
+    background-color : transparent;
+    margin: 0px 10px 20px 10px; //상 우 하 좌
+`;
 
 
 
@@ -48,6 +56,8 @@ const tempClubs = [
     { id: 3, name: `로딩중`, mainImg: `WinLockImages/a48b65589f2727feb93b12693ffeccb5d4aa1c0b6bbc1dff4d503ff28eba5a4c.jpg`, location: `수원시 영통구 매탄4동 10`, numMember: 10 },
 ];
 
+const tempSearchHistory = ['이거', '저거', '으아'];
+
 const SearchGroups = ({route, navigation}) => {
 
     const [clubs, setClubs] = useState(tempClubs);
@@ -55,7 +65,7 @@ const SearchGroups = ({route, navigation}) => {
 
     /* 검색 메인 창에는 상위 두 개의 결과만 표시 */
     useEffect(() => {
-        if (clubs.length > 2) {
+        if (clubs && clubs.length > 2) {
             SetShowingClubs(clubs.slice(0,2));
         } else {
             SetShowingClubs(clubs);
@@ -73,24 +83,34 @@ const SearchGroups = ({route, navigation}) => {
                     <Icon2 name="filter-alt" size={26} flex={2} onPress={() => alert('Filter!')} />
                     <Icon2 name="search" size={26} flex={2} onPress={() => alert('Search!')} />
                 </Header>
-                <Title>공동체 {clubs.length} 개</Title>
+                
+                {clubs == null && 
+                <KeywordView>
+                    {tempSearchHistory ? tempSearchHistory.map((v, i) => <TagBox key={i} text={v} color="blue" onPressDelBtn={() => setContent((current) => {let newContent = {...current}; newContent.keyword.splice(i, 1); return newContent})}/>) : null}
+                </KeywordView>}
 
-                {showingClubs.map((v, i) => (
-                    <>
-                        <ClubCard club={v} style={{ marginBottom: '10px' }} />
-                        <View style={{ height: 20, width: '100%', backgroundColor: 'transparent' }} />
-                    </>))}
+                {clubs && <>
+                
+                    <Title>공동체 {clubs.length} 개</Title>
 
-                <View style={{ width: "90%" }}>
-                    <Button title="더 보기" onPress={() => { navigation.navigate('ShowMoreClubs', { title: `공동체 검색 결과 ${clubs.length} 개`, clubs: clubs, navigation: navigation }); }} />
-                </View>
+                    {showingClubs.map((v, i) => (
+                        <>
+                            <ClubCard club={v} style={{ marginBottom: '10px' }} />
+                            <View style={{ height: 20, width: '100%', backgroundColor: 'transparent' }} />
+                        </>))}
 
+                    <View style={{ width: "90%" }}>
+                        <Button title="더 보기" onPress={() => { navigation.navigate('ShowMoreClubs', { title: `공동체 검색 결과 ${clubs.length} 개`, clubs: clubs, navigation: navigation }); }} />
+                    </View>
+                </>
+                }
                 <Separator />
 
-                <Title>번개 {clubs.length} 개</Title>
+                {clubs && <><Title>번개 {clubs ? clubs.length : 0} 개</Title>
                 <View style={{ width: "90%" }}>
                     <Button title="더 보기" onPress={() => alert('번개 모임 더보기')} />
                 </View>
+                </>}
             </View>
         </ScrollView>
     );
