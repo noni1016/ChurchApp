ac5e34d52c17ebd2bfa944c12d8ee76de3bfb433
noni1016 <play.noni@gmail.com>
2022-10-16 13:33:10 +0900

    'CHUR-131' 유저프로필00_프로필 페이지 헤더 - 유저이름, 신고버튼, 대화버튼, 환경설정 버튼

--- 02_UI/src/Screen/02_Main/03_Profile/Profile.js
+++ 02_UI/src/Screen/02_Main/03_Profile/Profile.js
@@ -1,13 +1,23 @@
 import React, {useState, useEffect, useContext} from 'react';
 import { View, Text } from 'react-native';
+import styled from 'styled-components/native';
 import {DomainContext} from '~/Context/Domain';
 import {UserData} from '~/Context/User';
 import {
     createStackNavigator,
 } from '@react-navigation/stack';
 
+import Icon1 from 'react-native-vector-icons/Ionicons';
+import Icon2 from 'react-native-vector-icons/AntDesign';
+
 const tempUser = {id: 4, name: "짱쎄", photo: 'Profile/짱쎄.jpg', role: 'user'};
 const Stack = createStackNavigator();
+
+const HeaderButtonsContainer = styled.View`
+    flex-direction: row;
+    justify-content: space-evenly;
+    width: 120px;
+`;
 
 const ProfileMain = ({navigation, route}) => {
     const domain = useContext(DomainContext);
@@ -21,7 +31,17 @@ const ProfileMain = ({navigation, route}) => {
     useEffect(() => {
         if (member)
         {
-            navigation.setOptions({title: member.name});
+            navigation.setOptions({
+                title: member.name,
+                headerShown: true,
+                headerRight: () => (
+                    <HeaderButtonsContainer>
+                        {userData.id != member.id && <Icon2 name="notification" size={26} onPress={() => alert('신고하기')} />}
+                        <Icon2 name="message1" size={26} onPress={() => alert('채팅')} />
+                        {userData.id == member.id && <Icon1 name="settings-outline" size={26} onPress={() => alert('프로필 수정')} />}
+                    </HeaderButtonsContainer>
+                )
+            });
         }
     }, [member])
 
