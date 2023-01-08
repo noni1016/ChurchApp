bbb3dc1d947d22028780508b9943b52ce3bf62bc
noni1016 <play.noni@gmail.com>
2023-01-08 13:14:18 +0900

    서버 IP 주소 변경

--- 02_UI/src/Context/Domain.js
+++ 02_UI/src/Context/Domain.js
@@ -1,11 +1,11 @@
 import React, {createContext} from 'react';
 
-const DomainContext = createContext('http://175.212.209.93:7009');
+const DomainContext = createContext('http://121.139.124.10:7009');
 
 const DomainContextProvider = ({children}) => {
 
     return (
-        <DomainContext.Provider value={'http://175.212.209.93:7009'}>
+        <DomainContext.Provider value={'http://121.139.124.10:7009'}>
             {children}
         </DomainContext.Provider>
     )
