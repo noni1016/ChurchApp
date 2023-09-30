a77c27e278cc20d729e09435c15ac89f6cdf4af6
noni1016 <play.noni@gmail.com>
2023-09-30 11:37:04 +0900

    'CHUR-65' 새 모임 만들기 UI 정리

--- 02_UI/src/Style.js
+++ 02_UI/src/Style.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
+import Styled from 'styled-components/native';
 
 const Styles = StyleSheet.create({
     header: {
@@ -7,7 +8,20 @@ const Styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 30,
         fontFamily: 'DoHyeon-Regular',
+    },
+
+    default: {
+        color: 'black',
+        fontSize: 20,
+        fontFamily: 'DoHyeon-Regular',
     }
 })
 
+const VerticalMargin = Styled.View`
+    width: 100%;
+    height: 100px;
+    background-color: transparent;
+`;
+
 export default Styles;
+export {VerticalMargin};
